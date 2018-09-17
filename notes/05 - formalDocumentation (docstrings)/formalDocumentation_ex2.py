10cf1c93915f5d01691084d15fa62f43b10071cc
Andrew Seidel <mrseidel@users.noreply.github.com>
2018-09-17 09:14:25 -0400

    Create formalDocumentation_ex2.py
    This is part of the split from the original single-example file.

---
+++ notes/05 - formalDocumentation (docstrings)/formalDocumentation_ex2.py
@@ -0,0 +1,35 @@
+#-----------------------------------------------------------------------------
+# Name:        Formal Documentation i.e. docstrings (formalDocumentation.py)
+# Purpose:     To provides an example of how to create docstrings in Python using
+#			            formal documentation standards.				
+#
+# Author:      Mr. Seidel
+# Created:     22-Aug-2018
+# Updated:     17-Sep-2018
+#-----------------------------------------------------------------------------
+
+def hypotenuse(sideA, sideB):
+	'''
+	Calculates the hypotenuse and returns it to the sender based on
+	sideA and sideB given
+	
+	Parameters
+	----------
+	sideA : float
+		One of the arms of the right angle of the triangle
+	sideB : float
+		The other arm of the right angle of the triangle
+	
+	Returns
+	-------
+	float
+		The hypotenuse value
+	'''
+
+	cSquare = sideA**2 + sideB**2	# local variable used to hold information
+	hypotenuseValue = cSquare**0.5 	# takes the square root
+	return float(hypotenuseValue)
+
+# Program runs starting here.  Above this line, the functions are just defined.
+hyp = hypotenuse(3, 4)
+print(str(hyp))
