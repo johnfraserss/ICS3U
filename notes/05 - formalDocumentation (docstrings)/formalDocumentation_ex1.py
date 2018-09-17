aa4f36af4b6c12fa0b3bcb014923c03351ac5825
Andrew Seidel <mrseidel@users.noreply.github.com>
2018-09-17 09:13:22 -0400

    Split examples to multiple files. Less confusion.

---
+++ notes/05 - formalDocumentation (docstrings)/formalDocumentation_ex1.py
@@ -0,0 +1,33 @@
+#-----------------------------------------------------------------------------
+# Name:        Formal Documentation i.e. docstrings (formalDocumentation.py)
+# Purpose:     To provides an example of how to create docstrings in Python using
+#			   formal documentation standards.				
+#
+# Author:      Mr. Seidel
+# Created:     22-Aug-2018
+# Updated:     17-Sep-2018
+#-----------------------------------------------------------------------------
+
+def menu():
+	'''
+	A menu of options
+	
+	This function will print out a list of food options
+	available for the user and then return to the caller
+	
+	Parameters
+	----------
+	None
+	
+	Returns
+	-------
+	None
+	'''
+
+	print('Menu')
+	print('Vegetables')
+	print('Fruit')
+	return
+
+# Program runs starting here.  Above this line, the functions are just defined.
+menu()
