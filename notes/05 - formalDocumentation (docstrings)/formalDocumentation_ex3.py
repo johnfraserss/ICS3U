e76039f07746561eb9af2fb6d321a642af7a7bc7
Andrew Seidel <mrseidel@users.noreply.github.com>
2018-09-17 09:16:46 -0400

    Create formalDocumentation_ex3.py
    This is part of the split from the original single-example file.

---
+++ notes/05 - formalDocumentation (docstrings)/formalDocumentation_ex3.py
@@ -0,0 +1,38 @@
+#-----------------------------------------------------------------------------
+# Name:        Formal Documentation i.e. docstrings (formalDocumentation_ex3.py)
+# Purpose:     To provides an example of how to create docstrings in Python using
+#			   formal documentation standards.				
+#
+# Author:      Mr. Seidel
+# Created:     22-Aug-2018
+# Updated:     17-Sep-2018
+#-----------------------------------------------------------------------------
+
+def printPerson(name, age=0):
+	'''
+	Prints out the information given about a person.
+	
+	Takes in information from the user and then prints out
+	a summary of the name and age of the information.  If 
+	the age isn't given, then it is defaulted to zero.
+	
+	Parameters
+	----------
+	name : str
+		The name of the person to be printed
+	age : int, optional
+		The age of the person to be printed. If nothing given,
+		value will be zero.
+	
+	Returns
+	-------
+	None
+	'''
+
+	print('Name: ' + str(name))
+	print('Age: ' + str(age))
+	return
+
+# Program runs starting here.  Above this line, the functions are just defined.
+printPerson('Einstein', 40)
+printPerson('Newborn')		# used the default value of the "age" parameter
