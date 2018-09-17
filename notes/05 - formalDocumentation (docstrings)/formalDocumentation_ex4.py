a8e7015e70680cc399c3d3b52bd253528a6aa797
Andrew Seidel <mrseidel@users.noreply.github.com>
2018-09-17 09:17:45 -0400

    Create formalDocumentation_ex4.py
    This is part of the split from the original single-example file.

---
+++ notes/05 - formalDocumentation (docstrings)/formalDocumentation_ex4.py
@@ -0,0 +1,39 @@
+#-----------------------------------------------------------------------------
+# Name:        Formal Documentation i.e. docstrings (formalDocumentation_ex4.py)
+# Purpose:     To provides an example of how to create docstrings in Python using
+#			              formal documentation standards.				
+#
+# Author:      Mr. Seidel
+# Created:     22-Aug-2018
+# Updated:     17-Sep-2018
+#-----------------------------------------------------------------------------
+
+def printEmployee(name, occupation, salary):
+	'''
+	Prints out the information given about an employee.
+	
+	Takes in information from the user and then prints out
+	a summary of the name, occupation, and salary of the 
+	information given.
+	
+	Parameters
+	----------
+	name : str
+		The name of the person to be printed
+	occupation : str
+		The occupation of the person to be printed
+	salary : float
+		The salary of the person to be printed.
+	
+	Returns
+	-------
+	None
+	'''
+	
+	print('Name: ' + str(name))
+	print('Occupation: ' + str(occupation))
+	print('Salary: ' + str(salary))
+
+# Program runs starting here.  Above this line, the functions are just defined.
+printEmployee(salary=31000, name="January", occupation="Month") # note order
+printEmployee(name="February", occupation="Month", salary=28000) # note order
