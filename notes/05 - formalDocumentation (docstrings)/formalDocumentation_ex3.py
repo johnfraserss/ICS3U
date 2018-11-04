1059116bf251f921a75b86bc2efebace21073787
Andrew Seidel <mrseidel@users.noreply.github.com>
2018-11-04 16:13:12 -0500

    Fixed spacing

--- notes/05 - formalDocumentation (docstrings)/formalDocumentation_ex3.py
+++ notes/05 - formalDocumentation (docstrings)/formalDocumentation_ex3.py
@@ -5,33 +5,32 @@
 #
 # Author:      Mr. Seidel
 # Created:     22-Aug-2018
-# Updated:     17-Sep-2018
+# Updated:     04-Nov-2018 (fixed spacing)
 #-----------------------------------------------------------------------------
 
 def printPerson(name, age=0):
-	'''
-	Prints out the information given about a person.
+  '''
+  Prints out the information given about a person.
 	
-	Takes in information from the user and then prints out
-	a summary of the name and age of the information.  If 
-	the age isn't given, then it is defaulted to zero.
+  Takes in information from the user and then prints out
+  a summary of the name and age of the information.  If 
+  the age isn't given, then it is defaulted to zero.
 	
-	Parameters
-	----------
-	name : str
-		The name of the person to be printed
-	age : int, optional
-		The age of the person to be printed. If nothing given,
-		value will be zero.
+  Parameters
+  ----------
+  name : str
+    The name of the person to be printed
+  age : int, optional
+    The age of the person to be printed. If nothing given, value will be zero.
 	
-	Returns
-	-------
-	None
-	'''
+  Returns
+  -------
+  None
+  '''
 
-	print('Name: ' + str(name))
-	print('Age: ' + str(age))
-	return
+  print('Name: ' + str(name))
+  print('Age: ' + str(age))
+  return
 
 # Program runs starting here.  Above this line, the functions are just defined.
 printPerson('Einstein', 40)
