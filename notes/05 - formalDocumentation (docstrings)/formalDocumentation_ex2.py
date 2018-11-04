2c14197384f0b4950f397be01614267cfb75446a
Andrew Seidel <mrseidel@users.noreply.github.com>
2018-11-04 16:07:58 -0500

    Fixed spacing

--- notes/05 - formalDocumentation (docstrings)/formalDocumentation_ex2.py
+++ notes/05 - formalDocumentation (docstrings)/formalDocumentation_ex2.py
@@ -5,30 +5,30 @@
 #
 # Author:      Mr. Seidel
 # Created:     22-Aug-2018
-# Updated:     17-Sep-2018
+# Updated:     04-Nov-2018 (fixed spacing)
 #-----------------------------------------------------------------------------
 
 def hypotenuse(sideA, sideB):
-	'''
-	Calculates the hypotenuse and returns it to the sender based on
-	sideA and sideB given
+  '''
+  Calculates the hypotenuse and returns it to the sender based on
+  sideA and sideB given
 	
-	Parameters
-	----------
-	sideA : float
-		One of the arms of the right angle of the triangle
-	sideB : float
-		The other arm of the right angle of the triangle
+  Parameters
+  ----------
+  sideA : float
+    One of the arms of the right angle of the triangle
+  sideB : float
+    The other arm of the right angle of the triangle
 	
-	Returns
-	-------
-	float
-		The hypotenuse value
-	'''
+  Returns
+  -------
+  float
+    The hypotenuse value
+  '''
 
-	cSquare = sideA**2 + sideB**2	# local variable used to hold information
-	hypotenuseValue = cSquare**0.5 	# takes the square root
-	return float(hypotenuseValue)
+  cSquare = sideA**2 + sideB**2	# local variable used to hold information
+  hypotenuseValue = cSquare**0.5 	# takes the square root
+  return float(hypotenuseValue)
 
 # Program runs starting here.  Above this line, the functions are just defined.
 hyp = hypotenuse(3, 4)
