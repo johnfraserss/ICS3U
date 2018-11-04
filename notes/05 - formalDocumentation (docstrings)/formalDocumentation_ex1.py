3fd9e61123e3d864a2d0a7f927684045555d4dbb
Andrew Seidel <mrseidel@users.noreply.github.com>
2018-11-04 16:12:24 -0500

    Fixed spacing

--- notes/05 - formalDocumentation (docstrings)/formalDocumentation_ex1.py
+++ notes/05 - formalDocumentation (docstrings)/formalDocumentation_ex1.py
@@ -5,29 +5,29 @@
 #
 # Author:      Mr. Seidel
 # Created:     22-Aug-2018
-# Updated:     17-Sep-2018
+# Updated:     04-Nov-2018 (fixed spacing)
 #-----------------------------------------------------------------------------
 
 def menu():
-	'''
-	A menu of options
+  '''
+  A menu of options
 	
-	This function will print out a list of food options
-	available for the user and then return to the caller
-	
-	Parameters
-	----------
-	None
-	
-	Returns
-	-------
-	None
-	'''
+  This function will print out a list of food options
+  available for the user and then return to the caller
 
-	print('Menu')
-	print('Vegetables')
-	print('Fruit')
-	return
+  Parameters
+  ----------
+  None
+	
+  Returns
+  -------
+  None
+  '''
+
+  print('Menu')
+  print('Vegetables')
+  print('Fruit')
+  return
 
 # Program runs starting here.  Above this line, the functions are just defined.
 menu()
