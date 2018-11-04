e12459c85d08972278059c65025d79e9b14ecf46
Andrew Seidel <mrseidel@users.noreply.github.com>
2018-11-04 16:13:58 -0500

    Fixed spacing

--- notes/05 - formalDocumentation (docstrings)/formalDocumentation_ex4.py
+++ notes/05 - formalDocumentation (docstrings)/formalDocumentation_ex4.py
@@ -5,34 +5,34 @@
 #
 # Author:      Mr. Seidel
 # Created:     22-Aug-2018
-# Updated:     17-Sep-2018
+# Updated:     04-Nov-2018 (fixed spacing)
 #-----------------------------------------------------------------------------
 
 def printEmployee(name, occupation, salary):
-	'''
-	Prints out the information given about an employee.
+  '''
+  Prints out the information given about an employee.
 	
-	Takes in information from the user and then prints out
-	a summary of the name, occupation, and salary of the 
-	information given.
+  Takes in information from the user and then prints out
+  a summary of the name, occupation, and salary of the 
+  information given.
 	
-	Parameters
-	----------
-	name : str
-		The name of the person to be printed
-	occupation : str
-		The occupation of the person to be printed
-	salary : float
-		The salary of the person to be printed.
+  Parameters
+  ----------
+  name : str
+    The name of the person to be printed
+  occupation : str
+    The occupation of the person to be printed
+  salary : float
+    The salary of the person to be printed.
+
+  Returns
+  -------
+  None
+  '''
 	
-	Returns
-	-------
-	None
-	'''
-	
-	print('Name: ' + str(name))
-	print('Occupation: ' + str(occupation))
-	print('Salary: ' + str(salary))
+  print('Name: ' + str(name))
+  print('Occupation: ' + str(occupation))
+  print('Salary: ' + str(salary))
 
 # Program runs starting here.  Above this line, the functions are just defined.
 printEmployee(salary=31000, name="January", occupation="Month") # note order
