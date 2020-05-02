3c7b65f7ab8478a3fb89eb032a029f990c07f0c2
mrseidel <mrseidel@users.noreply.github.com>
2020-05-02 19:12:48 -0400

    Fixed a few typos, updated log output

--- notes/21 - logging/logging_ex2a.py
+++ notes/21 - logging/logging_ex2a.py
@@ -5,7 +5,7 @@
 #
 # Author:      Mr. Seidel
 # Created:     11-Nov-2018
-# Updated:     26-Nov-2018
+# Updated:     02-May-2020 (removed assertions, updated for NoneType)
 #-----------------------------------------------------------------------------
 
 # These two lines are necessary to import the logging module
@@ -36,11 +36,11 @@ def multiply(numbers):
     logging.debug('Ensuring input is a list')
     if not isinstance(numbers, list):
         return None
-    logging.debug('Ensuring each item in the list is an int or float')
+    logging.debug("Outputting all items in the list to ensure they're ints or floats")
     for item in numbers:
         logging.debug('Checking ' + str(item) + ' is an int or a float')
 
-    logging.debug('All values are numbers, starting to multiply them all')
+    logging.debug('Starting to multiply all numbers')
 
     total = 0.0 # This is an intentional error to show the importance of debugging
     for item in numbers:
@@ -48,10 +48,8 @@ def multiply(numbers):
         total *= item
         logging.debug("Total's new value is " + str(total))
 
-    logging.debug('Ensuring the final value is a float value')
-
     logging.debug('Final value of total is ' + str(total))
-    logging.info('End of program')
+    logging.info('End of function')
     return total
     
 
@@ -62,37 +60,36 @@ logging.info('End of program')
 '''
  Below is the output from having total = 0.0 when it first is created (instead of a better value of 1.0)
 
- 2018-11-11 12:51:23,289 - INFO - Start of program
- 2018-11-11 12:51:23,301 - INFO - Starting multiply function
- 2018-11-11 12:51:23,306 - DEBUG - Ensuring input is a list
- 2018-11-11 12:51:23,311 - DEBUG - Ensuring each item in the list is an int or float
- 2018-11-11 12:51:23,316 - DEBUG - Checking 1 is an int or a float
- 2018-11-11 12:51:23,321 - DEBUG - Checking 2 is an int or a float
- 2018-11-11 12:51:23,326 - DEBUG - Checking 3 is an int or a float
- 2018-11-11 12:51:23,331 - DEBUG - Checking 4 is an int or a float
- 2018-11-11 12:51:23,336 - DEBUG - Checking 5 is an int or a float
- 2018-11-11 12:51:23,341 - DEBUG - Checking 6 is an int or a float
- 2018-11-11 12:51:23,346 - DEBUG - Checking 7 is an int or a float
- 2018-11-11 12:51:23,351 - DEBUG - Checking 8 is an int or a float
- 2018-11-11 12:51:23,356 - DEBUG - All values are numbers, starting to multiply them all
- 2018-11-11 12:51:23,361 - DEBUG - Total is 0.0, now multiplying 1 to the total.
- 2018-11-11 12:51:23,366 - DEBUG - Total's new value is 0.0
- 2018-11-11 12:51:23,371 - DEBUG - Total is 0.0, now multiplying 2 to the total.
- 2018-11-11 12:51:23,376 - DEBUG - Total's new value is 0.0
- 2018-11-11 12:51:23,381 - DEBUG - Total is 0.0, now multiplying 3 to the total.
- 2018-11-11 12:51:23,387 - DEBUG - Total's new value is 0.0
- 2018-11-11 12:51:23,392 - DEBUG - Total is 0.0, now multiplying 4 to the total.
- 2018-11-11 12:51:23,397 - DEBUG - Total's new value is 0.0
- 2018-11-11 12:51:23,402 - DEBUG - Total is 0.0, now multiplying 5 to the total.
- 2018-11-11 12:51:23,407 - DEBUG - Total's new value is 0.0
- 2018-11-11 12:51:23,412 - DEBUG - Total is 0.0, now multiplying 6 to the total.
- 2018-11-11 12:51:23,417 - DEBUG - Total's new value is 0.0
- 2018-11-11 12:51:23,423 - DEBUG - Total is 0.0, now multiplying 7 to the total.
- 2018-11-11 12:51:23,428 - DEBUG - Total's new value is 0.0
- 2018-11-11 12:51:23,433 - DEBUG - Total is 0.0, now multiplying 8 to the total.
- 2018-11-11 12:51:23,438 - DEBUG - Total's new value is 0.0
- 2018-11-11 12:51:23,443 - DEBUG - Ensuring the final value is a float value
- 2018-11-11 12:51:23,449 - DEBUG - Final value of total is 0.0
- 2018-11-11 12:51:23,449 - INFO - End of function
- 2018-11-11 12:51:23,454 - INFO - End of program
+ 2020-05-02 23:12:11,882 - INFO - Start of program
+ 2020-05-02 23:12:11,882 - INFO - Starting multiply function
+ 2020-05-02 23:12:11,882 - DEBUG - Ensuring input is a list
+ 2020-05-02 23:12:11,882 - DEBUG - Outputting all items in the list to ensure they're ints or floats
+ 2020-05-02 23:12:11,882 - DEBUG - Checking 1 is an int or a float
+ 2020-05-02 23:12:11,883 - DEBUG - Checking 2 is an int or a float
+ 2020-05-02 23:12:11,883 - DEBUG - Checking 3 is an int or a float
+ 2020-05-02 23:12:11,883 - DEBUG - Checking 4 is an int or a float
+ 2020-05-02 23:12:11,883 - DEBUG - Checking 5 is an int or a float
+ 2020-05-02 23:12:11,883 - DEBUG - Checking 6 is an int or a float
+ 2020-05-02 23:12:11,883 - DEBUG - Checking 7 is an int or a float
+ 2020-05-02 23:12:11,883 - DEBUG - Checking 8 is an int or a float
+ 2020-05-02 23:12:11,883 - DEBUG - Starting to multiply all numbers
+ 2020-05-02 23:12:11,883 - DEBUG - Total is 0.0, now multiplying 1 to the total.
+ 2020-05-02 23:12:11,883 - DEBUG - Total's new value is 0.0
+ 2020-05-02 23:12:11,883 - DEBUG - Total is 0.0, now multiplying 2 to the total.
+ 2020-05-02 23:12:11,883 - DEBUG - Total's new value is 0.0
+ 2020-05-02 23:12:11,883 - DEBUG - Total is 0.0, now multiplying 3 to the total.
+ 2020-05-02 23:12:11,883 - DEBUG - Total's new value is 0.0
+ 2020-05-02 23:12:11,884 - DEBUG - Total is 0.0, now multiplying 4 to the total.
+ 2020-05-02 23:12:11,884 - DEBUG - Total's new value is 0.0
+ 2020-05-02 23:12:11,884 - DEBUG - Total is 0.0, now multiplying 5 to the total.
+ 2020-05-02 23:12:11,884 - DEBUG - Total's new value is 0.0
+ 2020-05-02 23:12:11,884 - DEBUG - Total is 0.0, now multiplying 6 to the total.
+ 2020-05-02 23:12:11,884 - DEBUG - Total's new value is 0.0
+ 2020-05-02 23:12:11,884 - DEBUG - Total is 0.0, now multiplying 7 to the total.
+ 2020-05-02 23:12:11,884 - DEBUG - Total's new value is 0.0
+ 2020-05-02 23:12:11,884 - DEBUG - Total is 0.0, now multiplying 8 to the total.
+ 2020-05-02 23:12:11,884 - DEBUG - Total's new value is 0.0
+ 2020-05-02 23:12:11,884 - DEBUG - Final value of total is 0.0
+ 2020-05-02 23:12:11,884 - INFO - End of function
+ 2020-05-02 23:12:11,884 - INFO - End of program
 '''
