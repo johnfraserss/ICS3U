0de53b47733472e97ff51d251d721d07b95d12aa
mrseidel <mrseidel@users.noreply.github.com>
2022-02-15 09:27:47 -0500

    placeholders for note linking

---
+++ notes/34 - sets/README.md
@@ -0,0 +1,7 @@
+# Notes (Sets)
+
+Sets are yet another data structure that we will be learning about this semester.  Other data structures we have learned are lists, tuples, and dictionaries.
+
+**This is currently a placeholder to allow note-linking from wiki.**
+
+To be updated in the near future.
---
+++ notes/34 - sets/sets.py
@@ -0,0 +1,10 @@
+#-----------------------------------------------------------------------------
+# Name:        Sets (sets.py)
+# Purpose:     To provide examples of how to use sets
+#
+# Author:      Mr. Seidel
+# Created:     15-Feb-2022
+# Updated:     15-Feb-2022
+#-----------------------------------------------------------------------------
+
+# this is currently a placeholder to allow note-linking from wiki.
