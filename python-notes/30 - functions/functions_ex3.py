ebdcf18507d7bc1640f2f7ffe970459c3cace1c3
mrseidel <mrseidel@users.noreply.github.com>
2024-11-24 12:46:36 -0500

    Fixed typo(s)

--- python-notes/30 - functions/functions_ex3.py
+++ python-notes/30 - functions/functions_ex3.py
@@ -1,6 +1,6 @@
 #-----------------------------------------------------------------------------
 # Name:        Functions (functions_ex3.py)
-# Purpose:     To provide examples of how for functions work in Python
+# Purpose:     To provide examples of how functions work in Python
 #              This example includes an optional parameter with a default value.
 #
 # Author:      Mr. Seidel
@@ -12,7 +12,6 @@ def greet(name, greeting="Hello"):
     """A function that returns a personalized greeting."""
     return f"{greeting}, {name}!"
 
-# Example usage
 print(greet("Person1"))
 print(greet("Person2", "Hi"))
 print(greet("Person3", "Hey"))
